f426847c9adab928c76e7a3d633a13c4d72643c8
plkpiotr <plk.piotr.pp@gmail.com>
2018-07-17 19:51:32 +0200

    Add remaining files

---
+++ src/main/java/com/herokuapp/erpmesbackend/ErpMesBackendApplication.java
@@ -0,0 +1,12 @@
+package com.herokuapp.erpmesbackend.erpmesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpMesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpMesBackendApplication.class, args);
+	}
+}
