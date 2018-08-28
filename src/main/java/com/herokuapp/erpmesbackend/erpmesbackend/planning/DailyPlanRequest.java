c72c7d9e4df35f114f5216c7642ae5b16bf32dbd
patsaf <patrycja.saferna@gmail.com>
2018-08-28 22:14:08 +0200

    BACKEND #11 Planning - planning tests

--- src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/planning/DailyPlanRequest.java
+++ src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/planning/DailyPlanRequest.java
@@ -2,11 +2,13 @@ package com.herokuapp.erpmesbackend.erpmesbackend.planning;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DailyPlanRequest {
 
     @NotNull
--- src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/planning/SpecialPlanRequest.java
+++ src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/planning/SpecialPlanRequest.java
@@ -2,12 +2,14 @@ package com.herokuapp.erpmesbackend.erpmesbackend.planning;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SpecialPlanRequest {
 
     @NotNull
