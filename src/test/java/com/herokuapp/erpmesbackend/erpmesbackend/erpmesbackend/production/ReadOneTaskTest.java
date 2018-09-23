5f3afca0777c6329f2fd4ed86a79a21450120e23
plkpiotr <plk.piotr.pp@gmail.com>
2018-09-24 00:19:32 +0200

    BACKEND-6 :: Little fix in SecurityConfig.java

--- src/test/java/com/herokuapp/erpmesbackend/erpmesbackend/erpmesbackend/production/ReadOneTaskTest.java
+++ src/test/java/com/herokuapp/erpmesbackend/erpmesbackend/erpmesbackend/production/ReadOneTaskTest.java
@@ -2,6 +2,7 @@ package com.herokuapp.erpmesbackend.erpmesbackend.erpmesbackend.production;
 
 import com.herokuapp.erpmesbackend.erpmesbackend.erpmesbackend.FillBaseTemplate;
 import com.herokuapp.erpmesbackend.erpmesbackend.production.dto.TaskDTO;
+import com.herokuapp.erpmesbackend.erpmesbackend.production.model.MonthlyReport;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,11 +25,10 @@ public class ReadOneTaskTest extends FillBaseTemplate {
     }
 
     @Test
-    public void checkIfResponseContainsTaskWithGivenId() {
-        for (int i = 0; i < 3; i++) {
-            ResponseEntity<TaskDTO> forEntity = restTemplate.exchange("/tasks/{id}", HttpMethod.GET,
-                    new HttpEntity<>(null, requestHeaders), TaskDTO.class, i + 1);
-            assertThat(forEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        }
+    public void checkIfResponseContainsReportWithGivenId() {
+        ResponseEntity<TaskDTO> forEntity = restTemplate.exchange("/tasks/{id}", HttpMethod.GET,
+                new HttpEntity<>(null, requestHeaders), TaskDTO.class, 7);
+        assertThat(forEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(forEntity.getBody().getId()).isEqualTo(7);
     }
 }
