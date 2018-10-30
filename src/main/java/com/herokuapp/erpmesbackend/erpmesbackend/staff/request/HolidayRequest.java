6addccf824596407883803915b6682851ac8faa5
patsaf <patrycja.saferna@gmail.com>
2018-10-30 22:40:34 +0100

    Minor updates

--- src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/staff/request/HolidayRequest.java
+++ src/main/java/com/herokuapp/erpmesbackend/erpmesbackend/staff/request/HolidayRequest.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Data
@@ -21,5 +22,16 @@ public class HolidayRequest {
 
     @NotNull
     private HolidayType holidayType;
+
+    public int calculateWorkingDaysOff() {
+        int days = 0;
+        for (int i = 0; i < duration; i++) {
+            if (startDate.plusDays(i).getDayOfWeek() != DayOfWeek.SUNDAY &&
+                    startDate.plusDays(i).getDayOfWeek() != DayOfWeek.SATURDAY) {
+                days++;
+            }
+        }
+        return days;
+    }
 
 }
